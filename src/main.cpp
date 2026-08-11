73d00ec2d7489c218cd80691e24092cc4e7afb59
Camille ORDRONNEAU <camille1.ordronneau@gmail.com>
2026-08-11 21:09:12 +0200

    Add font

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,59 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <vector>
+
+#define RES_PATH(X) "./resources/" X
 
 int window_width = 1200;
 int window_height = 800;
+
+enum class FontMode
+{
+    NONE,
+    BOLD
+};
+
+struct CustomFont
+{
+    float size;
+    FontMode mode;
+    Font font;
+};
+
+std::vector<CustomFont> font_pool;
+
+Font get_font(float size, FontMode mode)
+{
+    for (const CustomFont &font : font_pool)
+    {
+        if (font.size == size && font.mode == mode)
+        {
+            return font.font;
+        }
+    }
+    std::vector<int> codepoints;
+    for (int i = 32; i <= 126; i++)
+        codepoints.push_back(i);
+    for (int i = 160; i <= 255; i++)
+        codepoints.push_back(i);
+
+    const char *font_path = "";
+    if (mode == FontMode::NONE)
+    {
+        font_path = RES_PATH("consolas.ttf");
+    }
+    else if (mode == FontMode::BOLD)
+    {
+        font_path = RES_PATH("consolas_bold.ttf");
+    }
+
+    CustomFont custom_font;
+    custom_font.size = size;
+    custom_font.mode = mode;
+    custom_font.font = LoadFontEx(font_path, size, codepoints.data(), codepoints.size());
+    font_pool.push_back(custom_font);
+    return custom_font.font;
+}
 
 int main()
 {
@@ -14,9 +65,15 @@ int main()
         BeginDrawing();
 
         ClearBackground(BLACK);
-        DrawText("Hello World", 100, 100, 20, WHITE);
+        DrawTextEx(get_font(20.0f, FontMode::NONE), "Hello", {100.0f, 100.0f}, 20.0f, 0.0f, WHITE);
+        DrawTextEx(get_font(20.0f, FontMode::BOLD), "World", {100.0f, 150.0f}, 20.0f, 0.0f, WHITE);
 
         EndDrawing();
+    }
+
+    for (const CustomFont &custom_font : font_pool)
+    {
+        UnloadFont(custom_font.font);
     }
 
     CloseWindow();
