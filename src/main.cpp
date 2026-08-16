40be403287aa8b2a11332a78cee9210f3462102b
Camille ORDRONNEAU <camille1.ordronneau@gmail.com>
2026-08-16 14:25:03 +0200

    Only display first packages

--- src/main.cpp
+++ src/main.cpp
@@ -398,6 +398,10 @@ float draw_category_page_packages(float y)
         y += TextBox({40.0f, y}, package.version.c_str(), style_package_title);
         y += TextBox({70.0f, y}, package.synopsis.c_str(), style_package_subtitle);
         y += TextBox({70.0f, y}, package.description.c_str(), style_package_subtitle);
+        if (y > window_height)
+        {
+            break;
+        }
     }
 
     return initial_y - y;
